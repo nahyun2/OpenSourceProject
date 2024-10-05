4d22dd0fe6769db6af4321dd00a19406fda3f161
heehyeok <ghdgmlgur0514@gmail.com>
2024-10-05 17:20:24 +0900

    week3_practice

---
+++ 실습파일/hong-heehyeok/week4JavaPractice/TestIf.java
@@ -0,0 +1,10 @@
+public class TestIf {
+    public static void main (String args[]){
+        int jumsu = 80;
+        if(jumsu >= 60)
+        {
+            System.out.println("당신은 합격입니다.");
+        }
+        System.out.println("당신의 점수는"+jumsu +"입니다");
+    }
+}
