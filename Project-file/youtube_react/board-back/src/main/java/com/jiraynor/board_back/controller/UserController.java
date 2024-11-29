d130d56c321f9eb2ab223ceb32ffb2003f54741a
yeontteo <yszzivs@gmail.com>
2024-11-29 15:46:54 +0900

    36,37,75-78

--- Project-file/youtube_react/board-back/src/main/java/com/jiraynor/board_back/controller/UserController.java
+++ Project-file/youtube_react/board-back/src/main/java/com/jiraynor/board_back/controller/UserController.java
@@ -3,12 +3,19 @@ package com.jiraynor.board_back.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.jiraynor.board_back.dto.request.user.PatchNicknameRequestDto;
+import com.jiraynor.board_back.dto.request.user.PatchProfileImageRequestDto;
 import com.jiraynor.board_back.dto.response.user.GetSignInUserResponseDto;
+import com.jiraynor.board_back.dto.response.user.PatchNicknameResponseDto;
+import com.jiraynor.board_back.dto.response.user.PatchProfileImageResponseDto;
 import com.jiraynor.board_back.service.UserService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -24,6 +31,24 @@ public class UserController {
         @AuthenticationPrincipal String email
     ) {
         ResponseEntity<? super GetSignInUserResponseDto> response = userService.getSignInUser(email);
+        return response;
+    }
+
+    @PatchMapping("/nickname")
+    public ResponseEntity<? super PatchNicknameResponseDto> patchNickname(
+        @RequestBody @Valid PatchNicknameRequestDto requestBody,
+        @AuthenticationPrincipal String email
+    ) {
+        ResponseEntity<? super PatchNicknameResponseDto> response = userService.patchNickname(requestBody, email);
+        return response;
+    }
+
+    @PatchMapping("/profile-image")
+    public ResponseEntity<? super PatchProfileImageResponseDto> patchProfileImage(
+        @RequestBody @Valid PatchProfileImageRequestDto requestBody,
+        @AuthenticationPrincipal String email
+    ) {
+        ResponseEntity<? super PatchProfileImageResponseDto> response = userService.patchProfileImage(requestBody, email);
         return response;
     }
 }
---
+++ Project-file/youtube_react/board-back/src/main/java/com/jiraynor/board_back/dto/request/user/PatchNicknameRequestDto.java
@@ -0,0 +1,15 @@
+package com.jiraynor.board_back.dto.request.user;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class PatchNicknameRequestDto {
+    
+    @NotBlank
+    private String nickname;
+}
